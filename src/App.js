d762359aba3af34b3a7e98086a5cbe99a39b19ae
sereigh <kb9286@gmail.com>
2023-09-16 19:19:46 -0600

    add font and theme

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home.js';
 import './App.css';
 import Test from './components/pages/Test/Test.js';
+import { createTheme, ThemeProvider } from '@mui/material';
 
-const App = () => (
-  <Switch>
-    <Route exact={true} path="/" component={Home} />
-    <Route exact={true} path="/test" component={Test} />
-  </Switch>
-);
+const App = () => {
+
+    const theme = createTheme({
+        typography: {
+          fontFamily: [
+            'Montserrat',
+          ].join(','),
+        },});
+    
+    return (
+        <ThemeProvider theme={theme}>
+            <Switch>
+                <Route exact={true} path="/" component={Home} />
+                <Route exact={true} path="/test" component={Test} />
+            </Switch>
+        </ThemeProvider>
+    )
+};
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -32,8 +32,11 @@ export const renderApp = (req, res) => {
   <head>
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta charset="utf-8" />
-      <title>Welcome to Razzle</title>
+      <title>Mile High Moments</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
       ${cssLinksFromAssets(assets, 'client')}
   </head>
   <body>
