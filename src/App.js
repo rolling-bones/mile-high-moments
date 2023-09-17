ae09fc1f35d75b47ba4340d7b8ecfc266e1a3b24
sereigh <kb9286@gmail.com>
2023-09-16 18:27:21 -0600

    add test component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './Home';
+import Home from './Home.js';
 import './App.css';
+import Test from './components/pages/Test/Test.js';
 
 const App = () => (
   <Switch>
     <Route exact={true} path="/" component={Home} />
+    <Route exact={true} path="/test" component={Test} />
   </Switch>
 );
 
---
+++ src/components/pages/Test/Test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './react.svg';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>Test Component Here</h1>
+      </>
+    );
+  }
+}
+
+export default Test;
